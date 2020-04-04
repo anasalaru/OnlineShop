b2e71643741f101770868ae99db8b8e8676a36ae
anamariasalaru <anamaria.salaru@thinslices.com>
2020-04-04 13:19:02 +0300

    implement share

--- app/src/main/java/com/anamaria/onlineshop/BookActivity.kt
+++ app/src/main/java/com/anamaria/onlineshop/BookActivity.kt
@@ -1,18 +1,18 @@
 package com.anamaria.onlineshop
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_book.*
 
 class BookActivity : AppCompatActivity() {
     private var bookId: String? = null
+    private lateinit var item: Book
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
@@ -21,13 +21,15 @@ class BookActivity : AppCompatActivity() {
 
         bookId = intent.getStringExtra("bookId")
 
-        val item: Book = items.find { it.id == bookId }!!
+        item = items.find { it.id == bookId }!!
 
         author.text = item.author
         book_title.text = item.title
         price.text = "${item.price} $"
         rating.text = "Rating: ${item.rating}"
         description.text = item.description
+
+        title = item.title
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -40,6 +42,25 @@ class BookActivity : AppCompatActivity() {
         val restoredBookId = savedInstanceState?.getString("bookId")
         Log.d(TAG, "restored book id is $restoredBookId")
         bookId = restoredBookId
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.share) {
+            shareBook()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareBook() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_SEND
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Check out this cool book I found: ${item.title} by ${item.author}. And it's only ${item.price}$! Go buy it Cheap Book Shop!"
+        )
+        intent.type = "text/plain"
+        startActivity(Intent.createChooser(intent, null))
     }
 
     override fun onStart() {
