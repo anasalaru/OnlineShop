ad471344897847d243bbb12fcbebee13f13706d6
anamariasalaru <anamaria.salaru@thinslices.com>
2020-04-11 12:00:13 +0300

    Add placeholder to imageview

--- app/src/main/java/com/anamaria/onlineshop/MyAccountActivity.kt
+++ app/src/main/java/com/anamaria/onlineshop/MyAccountActivity.kt
@@ -1,7 +1,10 @@
 package com.anamaria.onlineshop
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_my_account.*
 
 class MyAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,17 @@ class MyAccountActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_my_account)
         title = "My Account"
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Glide.with(this)
+            .load(Uri.EMPTY)
+            .centerCrop()
+            .placeholder(R.drawable.ic_account_circle)
+            .into(avatar)
+
+        name.text = "Username"
     }
 }
