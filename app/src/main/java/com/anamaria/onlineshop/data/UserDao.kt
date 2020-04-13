b7aaa3b5a43794a0ed418e72e8d0fe2dc1778402
anamariasalaru <anamaria.salaru@thinslices.com>
2020-04-13 20:05:26 +0300

    take picture & save in db

--- app/src/main/java/com/anamaria/onlineshop/data/UserDao.kt
+++ app/src/main/java/com/anamaria/onlineshop/data/UserDao.kt
@@ -2,29 +2,46 @@ package com.anamaria.onlineshop.data
 
 import android.content.Context
 import io.realm.Realm
+import io.realm.RealmConfiguration
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
 
 class UserDao(context: Context) {
-    private val realm: Realm
+    private lateinit var realm: Realm
+    private lateinit var realmConfig: RealmConfiguration
+
+    val bgThread = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 
     init {
-        Realm.init(context)
-        realm = Realm.getDefaultInstance()
+        CoroutineScope(bgThread).launch {
+            Realm.init(context)
+            realmConfig = RealmConfiguration.Builder().build()
+            realm = Realm.getInstance(realmConfig)
+        }
     }
 
-    suspend fun getMe(): User? =
-        withContext(IO) {
-            realm.where<User>(User::class.java).findFirst()
+    suspend fun getMe(): UserUIModel =
+        withContext(bgThread) {
+            val dbUser = realm.where<User>(User::class.java).findFirst()
+            UserMapper.dbToUI(dbUser)
         }
 
-    suspend fun persistMe(user: User) {
-        withContext(IO) {
+    suspend fun persistMe(user: UserUIModel) {
+        withContext(bgThread) {
+            val dbUser = UserMapper.uiToDb(user)
             with(realm) {
                 beginTransaction()
-                copyToRealm(user)
+                copyToRealm(dbUser)
                 commitTransaction()
             }
         }
     }
+}
+
+class UserUIModel(val name: String?, val avatar: ByteArray?)
+object UserMapper {
+    fun uiToDb(u: UserUIModel): User = User(u.name, u.avatar)
+    fun dbToUI(u: User?): UserUIModel = UserUIModel(u?.name, u?.avatar)
 }
